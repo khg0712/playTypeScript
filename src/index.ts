43e01cbf0b89f75dd22bb100632fcc194886cb42
psk <psk@midasit.com>
2020-03-27 20:10:39 +0900

    - 완전 좋은뎅

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,7 @@ class Index {
     keyName_test: string;
     @defineProperty(Rich.DEFINE_TYPE.STRING, {value: '태림이 천재2'})
     keyName_test2: string;
-
-
     constructor(person: Person) {
-
         Rich.LOOPER.addMainLoop('테스트루프', v => {
             // this.keyName_test=1
             console.log(this.keyName_test, this.keyName_test2)
